31fb43f91ffcc567ca8fc0fc418aa2016a6a7846
GabUrru <gurruela@estudiantec.cr>
2024-02-15 18:46:33 -0600

    Codigo con errores de flake8

---
+++ Tarea 1/Errores_flake8.py
@@ -0,0 +1,7 @@
+
+#Este comentario es un error al no dejar espacio despues del #
+flag = True
+# Este if no debe comparar el tipo de variable mediante un operador
+if type(flag) == bool:
+    # Este print contiene una linea demasiado larga
+    print("Hello world, this is an overly large message that will be detected by flake 8")
